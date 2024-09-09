8bf1e5df2254c555db6ea539740de30b9dd559fd
Nathan Lai <42527870+nathan-lai-engineering@users.noreply.github.com>
2024-09-08 23:22:53 -0700

    upload image as texture

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,28 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb/stb_image.h>	
 
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
+#include"Texture.h"
+
+// vertices to draw an equilateral triangle
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
+};
+
+GLuint indices[] =
+{
+	0, 2, 1, // Upper triangle
+	0, 3, 2 // Lower triangle
+};
 
 int main()
 {
@@ -16,24 +33,6 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// vertices to draw an equilateral triangle
-	GLfloat vertices[] =
-	{ //               COORDINATES                  /     COLORS           //
-		-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-		 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-		 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-		-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-		 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-		 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
-	};
-
-	GLuint indices[] =
-	{
-		0,3,5,
-		3,2,4,
-		5,4,1
-	};
 
 	// create a GLFW window object to put our stuffs on
 	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
@@ -69,13 +68,27 @@ int main()
 	// link VBO to VAO then unbind so its not modifiable
 	// openGL will automatically create gradients if vertices have different colors
 	// this is called interpolation
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	// parameters VBO, layout index, number of components, stride, and offset
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
+	// unbing to stop accidental modification
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
+	// get ID of uniform scale
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	// texture parammeters
+	int widthImg, heightImg, numColCh;
+
+	// stbi and opengl are reversed vertically so we must flip to right side up
+	stbi_set_flip_vertically_on_load(true);
+
+	Texture penguinTex("penguin.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	penguinTex.texUnit(shaderProgram, "tex0", 0);
 
 	while (!glfwWindowShouldClose(window)) 
 	{
@@ -85,13 +98,18 @@ int main()
 
 		// activate shader program
 		shaderProgram.Activate();
+
+		// assign value to uniform which must be done after activating shader program
 		glUniform1f(uniID, 0.5f);
+
+		// bind texture so it appears
+		penguinTex.Bind();
 
 		// bind the VAO to use this specifically
 		VAO1.Bind();
 
 		// specify primitive, starting index of vertices, and vertex count
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// swap to show changes
 		glfwSwapBuffers(window);
@@ -104,6 +122,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	penguinTex.Delete();
 	shaderProgram.Delete();
 
 	// clean up once done
---
+++ Texture.h
@@ -0,0 +1,30 @@
+#ifndef TEXTURE_CLASS_H
+#define TEXTURE_CLASS_H
+
+
+#include<glad/glad.h>
+#include<stb/stb_image.h>
+
+#include"shaderClass.h"
+
+class Texture 
+{
+public:
+	GLuint ID;
+	GLenum type;
+	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
+
+	// asign a texture unit (collection of like 16 textures) to this texture
+	void texUnit(Shader& shader, const char* uniform, GLuint unit);
+
+	// bind texture
+	void Bind();
+
+	// unbind texture
+	void Unbind();
+
+	// delete texture for cleanup
+	void Delete();
+};
+
+#endif
